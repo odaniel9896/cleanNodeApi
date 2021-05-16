64619b8c89e699cb14ffbbcf5d22e7a591d575c4
Daniel Vitor <soumenorgame@gmail.com>
2021-05-16 12:38:27 -0300

    test: ensure SingUpController returns 400 if no provider

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from './singup'
+describe('SingUp Controller', () => {
+  test('Should return 404 if no name is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
