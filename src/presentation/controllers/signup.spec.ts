788fe331bea4ae743587e64cf4d31fce7bcf9a7f
O Daniel <soumenorgame@gmail.com>
2021-06-06 10:22:28 -0300

    test: ensure SignUpController returns 400 if an invalid email is provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,61 +1,97 @@
-import { SignUpController } from './singup'
-import { MissigParamError } from '../errors/missing-param-error'
+import { SignUpController } from "./singup";
+import { MissigParamError } from "../errors/missing-param-error";
+import { InvalidParamError } from "../errors/invalid-param-error";
+import { EmailValidator } from "../protocols/email-validator";
 
-const makeSut = (): SignUpController => {
-  return new SignUpController
+interface SutTypes {
+  sut: SignUpController;
+  emailValidatorStub: EmailValidator;
 }
 
-describe('SingUp Controller', () => {
-  test('Should return 404 if no name is provider', () => {
-    const sut = makeSut()
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub();
+
+  const sut = new SignUpController(emailValidatorStub);
+  return {
+    sut,
+    emailValidatorStub,
+  };
+};
+
+describe("SingUp Controller", () => {
+  test("Should return 404 if no name is provider", () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissigParamError('name'))
-  })
-  test('Should return 404 if no email is provider', () => {
-    const sut = makeSut()
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissigParamError("name"));
+  });
+  test("Should return 404 if no email is provider", () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissigParamError('email'))
-  })
-  test('Should return 404 if no password is provider', () => {
-    const sut = makeSut()
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissigParamError("email"));
+  });
+  test("Should return 404 if no password is provider", () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        name: 'any_name',
-        email : 'any_email@gmail.com',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissigParamError('password'))
-  })
-  test('Should return 404 if no password confirmation is provider', () => {
-    const sut = makeSut()
+        name: "any_name",
+        email: "any_email@gmail.com",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissigParamError("password"));
+  });
+  test("Should return 404 if no password confirmation is provider", () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        name: 'any_name',
-        email : 'any_email@gmail.com',
-        password: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissigParamError('passwordConfirmation'))
-  })
-})
+        name: "any_name",
+        email: "any_email@gmail.com",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissigParamError("passwordConfirmation")
+    );
+  });
+  test("Should return 404 if if an invalid email is provider", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "invaliad_email@gmail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError("email"));
+  });
+});
